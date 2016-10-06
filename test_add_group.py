204e528af02cebc0a2fad5ec10074d8d70238741
peruana80 <kkulczycka@yahoo.com>
2016-10-06 17:15:51 +0200

    empty_group test added

--- test_add_group.py
+++ test_add_group.py
@@ -15,11 +15,19 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_test_add_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.create_group(wd, name="new", header="new", footer="new")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, name="", header="", footer="")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
