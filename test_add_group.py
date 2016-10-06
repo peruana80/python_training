dd30c67fd1185f5155c5a7a0480b4a2bef9c6536
peruana80 <kkulczycka@yahoo.com>
2016-10-06 13:29:27 +0200

    comments added

--- test_add_group.py
+++ test_add_group.py
@@ -16,9 +16,10 @@ class test_add_group(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_test_add_group(self):
-        success = True
         wd = self.wd
+        #Open home page
         wd.get("http://localhost/addressbook/group.php")
+        #login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -27,7 +28,11 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        #open groups page
+        wd.find_element_by_link_text("groups").click()
+        #create group
         wd.find_element_by_name("new").click()
+        #fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("new")
@@ -37,11 +42,13 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("new")
+        #submit group creation
         wd.find_element_by_name("submit").click()
+        #return to groups page
         wd.find_element_by_link_text("group page").click()
+        #logout
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
     def tearDown(self):
         self.wd.quit()
 
