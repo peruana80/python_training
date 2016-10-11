28b8fe801feee61beacf8b0e6bf87cbc123f7eb8
peruana80 <kkulczycka@yahoo.com>
2016-10-11 11:29:45 +0200

    old files removed

--- contact.py
+++ contact.py
@@ -1,7 +1,22 @@
 class Contact:
-    def __init__(self, firstname, lastname, address, home_number, email):
-        self.firstname=firstname
-        self.lastname=lastname
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home_number, mobile_number, work_number, fax, email, email2, email3, homepage, byear, ayear, address_2, phone2, notes):
+        self.first_name=first_name
+        self.middle_name=middle_name
+        self.last_name=last_name
+        self.nickname=nickname
+        self.title=title
+        self.company=company
         self.address=address
         self.home_number=home_number
+        self.mobile_number=mobile_number
+        self.work_number=work_number
+        self.fax=fax
         self.email=email
+        self.email2=email2
+        self.email3=email3
+        self.homepage=homepage
+        self.byear=byear
+        self.ayear=ayear
+        self.address_2=address_2
+        self.phone2=phone2
+        self.notes=notes
