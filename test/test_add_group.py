7e491024f8afdf69e24865a6dd84eeb8a7742833
peruana80 <kkulczycka@yahoo.com>
2016-10-15 23:02:26 +0200

    Class GroupHelper added for group test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -16,11 +16,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="new", header="new", footer="new"))
+    app.group.create(Group(name="new", header="new", footer="new"))
     app.session.logout()
 
 
 def test_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
