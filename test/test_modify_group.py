7d0dd6a0ab2ccfd98d8cbe2781927f6785efd147
peruana80 <kkulczycka@yahoo.com>
2016-10-23 22:59:55 +0200

    login/logout only one time

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,18 +1,12 @@
 from model.group import Group
 
 def test_modify_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="modyfikacja nazwy", header="modyfikacja naglowka", footer="modyfikacja stopki"))
-    app.session.logout()
 
 
 def test_modify_first_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="modyfikacja nazwy"))
-    app.session.logout()
 
 
 def test_modify_first_group_heder(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(header="modyfikacja naglowka"))
-    app.session.logout()
