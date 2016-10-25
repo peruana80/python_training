c1df2e323dfa737c7b548dcf9ac7b22f28aecf2b
peruana80 <kkulczycka@yahoo.com>
2016-10-25 10:36:55 +0200

    del/modify group/contact - ex 9 finished

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,6 +1,8 @@
 from model.contact import Contact
 
 def test_modify_first_contact(app):
+    if app.group.count() == 0:
+        app.contact.create(Contact(first_name="test"))
     app.contact.modify_first_contact(Contact(first_name="Zmodyfikuj imie", middle_name="Zmodyfikuj drugie imie", last_name="Zmodyfikuj nazwisko", nickname="Zmodyfikuj ksywe", title="Zmodyfikuj tytul",
                                         company="Zmodyfikuj firme", address="Zmodyfikuj adres", home_number="Zmodyfikuj telefon domowy", mobile_number="Zmodyfikuj telefon komorkowy",
                                         work_number="Zmodyfikuj telefon sluzbowy", fax="Zmodyfikuj fax", email="Zmodyfikuj email", email2="Zmodyfikuj email2", email3="Zmodyfikuj email3",
@@ -9,8 +11,12 @@ def test_modify_first_contact(app):
 
 
 def test_modify_first_contact_first_name(app):
+    if app.group.count() == 0:
+        app.contact.create(Contact(first_name="test"))
     app.contact.modify_first_contact(Contact(first_name="Zmodyfikuj imie"))
 
 
 def test_modify_first_contact_address(app):
+    if app.group.count() == 0:
+        app.contact.create(Contact(first_name="test"))
     app.contact.modify_first_contact(Contact(address="Zmodyfikuj adres"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,12 +1,18 @@
 from model.group import Group
 
 def test_modify_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="modyfikacja nazwy", header="modyfikacja naglowka", footer="modyfikacja stopki"))
 
 
 def test_modify_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="modyfikacja nazwy"))
 
 
 def test_modify_first_group_heder(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="modyfikacja naglowka"))
