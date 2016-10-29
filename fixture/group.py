9c16038d9d0018c7cc36628f039766fed7532b5a
peruana80 <kkulczycka@yahoo.com>
2016-10-29 22:58:33 +0200

    add/del groups lesson 4

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,6 @@
+from model.group import Group
+
+
 class GroupHelper:
 
 
@@ -68,3 +71,14 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups")
         return len(wd.find_elements_by_name("selected[]"))
+
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_group_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
