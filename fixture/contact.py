b7ab46ac3c4aa0292f0a9e190944c8514b5744f8
peruana80 <kkulczycka@yahoo.com>
2016-11-06 01:02:38 +0100

    test_phones - part of exercise done

--- fixture/contact.py
+++ fixture/contact.py
@@ -61,6 +61,36 @@ class ContactHelper:
         cell = table_row.find_elements_by_tag_name("td")[6]
         cell.find_element_by_tag_name("a").click()
 
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        first_name = wd.find_element_by_name("firstname").get_attribute("value")
+        last_name = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home_number = wd.find_element_by_name("home").get_attribute("value")
+        mobile_number = wd.find_element_by_name("mobile").get_attribute("value")
+        work_number = wd.find_element_by_name("work").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(first_name=first_name, last_name=last_name, id=id,
+                       home_number=home_number, mobile_number=mobile_number, work_number=work_number, phone2=phone2)
+
+    contact_cache = None
+
+    def get_contact_list(self):
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.open_main_page()
+            self.contact_cache = []
+            for element in wd.find_elements_by_name("entry"):
+                cells = element.find_elements_by_tag_name("td")
+                text1 = cells[1].text
+                text2 = cells[2].text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(first_name=text2, last_name=text1, id=id, home_number=all_phones[0],
+                                                  mobile_number=all_phones[1], work_number=all_phones[2], phone2=all_phones[3]))
+        return list(self.contact_cache)
 
     def change_filed_value(self, filed_name, text):
         if text is not None:
@@ -143,18 +173,4 @@ class ContactHelper:
         return len(wd.find_elements_by_name("selected[]"))
 
 
-    contact_cache = None
-
-    def get_contact_list(self):
-        if self.contact_cache is None:
-            wd = self.app.wd
-            self.open_main_page()
-            self.contact_cache = []
-            for element in wd.find_elements_by_name("entry"):
-                cells = element.find_elements_by_tag_name("td")
-                text1 = cells[1].text
-                text2 = cells[2].text
-                id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(first_name=text2, last_name=text1, id=id))
-        return list(self.contact_cache)
 
