191fb16b5f99e526e351d0c0d765b1c26363fef3
peruana80 <kkulczycka@yahoo.com>
2016-11-13 23:40:36 +0100

    Ex. 18 and Ex.19 finished

--- generator/contact.py
+++ generator/contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -24,7 +24,7 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*2
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -43,4 +43,5 @@ testdata = [Contact(first_name="", middle_name="", last_name="", nickname="", ti
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
-import pytest
-#from generator.contact import testdata
-from data.add_group import testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[str(x) for x in testdata])
-def test_add_new_contact(app, contact):
+def test_add_new_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
