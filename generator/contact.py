4ba7881a41757610bce1edfa11ddf736fbc08a42
peruana80 <kkulczycka@yahoo.com>
2016-11-13 01:00:51 +0100

    generacja testowych danych w formacie JSON

---
+++ generator/contact.py
@@ -0,0 +1,46 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(first_name="", middle_name="", last_name="", nickname="", title="", company="",
+                    address="", home_number="", mobile_number="", work_number="", fax="",
+                    email="", email2="", email3="", homepage="", byear="", ayear="",
+                    address_2="", phone2="", notes="")] + [
+        Contact(first_name=random_string("first_name",10), middle_name=random_string("middle_name",10), last_name=random_string("last_name",10),nickname=random_string("nickname",10),
+                title = random_string("title", 10), company=random_string("company",10), address=random_string("address",10), home_number=random_string("home_number",10),
+                mobile_number=random_string("mobile_number",10), work_number=random_string("work_number",10), fax=random_string("fax",10), email=random_string("email",10),
+                email2=random_string("email2",10), email3=random_string("email3",10), homepage=random_string("homepage",10), byear=random_string("byear",10), ayear=random_string("ayear",10),
+                address_2=random_string("address_2",10), phone2=random_string("phone2",10), notes=random_string("notes",10))
+        for i in range(5)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
+
 from model.contact import Contact
 import pytest
-import random
-import string
+from generator.contact import testdata
 
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(first_name="", middle_name="", last_name="", nickname="", title="", company="",
-                    address="", home_number="", mobile_number="", work_number="", fax="",
-                    email="", email2="", email3="", homepage="", byear="", ayear="",
-                    address_2="", phone2="", notes="")] + [
-        Contact(first_name=random_string("first_name",5), middle_name=random_string("middle_name",5), last_name=random_string("last_name",5),
-                nickname=random_string("nickname", 5),title=random_string("title",5),company=random_string("company",5),
-                address=random_string("address", 5),home_number=random_string("home_number",5),mobile_number=random_string("mobile_number",5),
-                work_number=random_string("work_number", 5),fax=random_string("fax",5),email=random_string("email",5),
-                email2=random_string("email2", 5),email3=random_string("email3",5),homepage=random_string("homepage",5),byear=random_string("byear",5),
-                ayear=random_string("ayear", 5),address_2=random_string("address_2",5),phone2=random_string("phone2",5),notes=random_string("notes",5))
-        for i in range(5)
-]
 
 @pytest.mark.parametrize("contact", testdata, ids=[str(x) for x in testdata])
 def test_add_new_contact(app, contact):
