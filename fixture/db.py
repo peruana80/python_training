53563906a68f9a253ed7b697c40439cc5f336cf6
peruana80 <kkulczycka@yahoo.com>
2016-11-23 16:51:05 +0100

    jekcja 7 filmik 4

--- fixture/db.py
+++ fixture/db.py
@@ -10,6 +10,7 @@ class DbFixture():
         self.user = user
         self.password = password
         self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+        self.connection.autocommit = True
 
     def get_group_list(self):
         list = []
@@ -23,17 +24,17 @@ class DbFixture():
             cursor.close()
         return list
 
-    def get_contact_list(self):
-        list = []
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00")
-            for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
-        finally:
-            cursor.close()
-        return list
+ #   def get_contact_list(self):
+ #       list = []
+ #       cursor = self.connection.cursor()
+ #       try:
+ #           cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00")
+ #           for row in cursor:
+  #              (id, firstname, lastname) = row
+  #              list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+  #      finally:
+  #          cursor.close()
+  #      return list
 
 
     def destroy(self):
