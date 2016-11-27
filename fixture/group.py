29137f963df0fbfe10ee3ed1f1dc6f66f7f7993b
peruana80 <kkulczycka@yahoo.com>
2016-11-27 23:16:01 +0100

    Revert "sql - test add group/test del group"
    This reverts commit b9176416ab682d39cb245ad1bcf5a4fbfc11d6af.

--- fixture/group.py
+++ fixture/group.py
@@ -22,10 +22,6 @@ class GroupHelper:
         wd.find_elements_by_name("selected[]")[index].click()
 
 
-    def select_group_by_id(self, id):
-        wd = self.app.wd
-        wd.find_element_by_css_selector("input[value='%s']" %id).click()
-
     def delete_first_group(self):
         self.delete_group_by_index(0)
 
@@ -38,14 +34,6 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def delete_group_by_id(self, id):
-        wd = self.app.wd
-        self.open_group_page()
-        self.select_group_by_id(id)
-        #submit deletion
-        wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
-        self.group_cache = None
 
     def open_group_page(self):
         wd = self.app.wd
