e514c4afa52647bc7f4787ed292ef62041602cac
peruana80 <kkulczycka@yahoo.com>
2016-11-28 10:46:11 +0100

    errors

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,10 +1,12 @@
 from fixture.db import DbFixture
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password='')
+
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
 try:
-    groups = db.get_group_list()
-    for group in groups:
-        print(group)
-    print(len(groups))
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
 finally:
     db.destroy()
